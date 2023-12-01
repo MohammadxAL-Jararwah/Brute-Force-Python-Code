d67f75ce09d35fe42763f0ad0a43f74afa9a13b0
MohammadxAL-Jararwah <149402460+MohammadxAL-Jararwah@users.noreply.github.com>
2023-12-01 17:35:17 +0300

    Update Brute Force python code.py

--- Brute Force python code.py
+++ Brute Force python code.py
@@ -5,17 +5,13 @@ len_pass = int(input("len_pass = "))
 len_pass = [0] * len_pass
 
 def count_slice(lis):
-    counter = len(lis) - 1
+    index = len(lis) - 1
+    
+    while lis[index] >= (len(letters) - 1):
+        lis[index] = 0
+        index -= 1
 
-    for i in range(len(lis)-1,0,-1):
-        if lis[i] >= len(letters)-1:
-            lis[i] = 0
-            counter -= 1
-        else:
-            break
-
-    lis[counter] = (lis[counter] + 1) % len(letters)
-
+    lis[index] = (lis[index] + 1) % len(letters)
     return lis
 
 while True:
