51707f9de88b5be71cefe522b2d1044da5613c24
MohammadxAL-Jararwah <149402460+MohammadxAL-Jararwah@users.noreply.github.com>
2023-12-01 17:42:15 +0300

    Update Brute Force python code.py

--- Brute Force python code.py
+++ Brute Force python code.py
@@ -6,7 +6,7 @@ len_pass = [0] * len_pass
 
 def count_slice(lis):
     index = len(lis) - 1
-    
+
     while lis[index] >= (len(letters) - 1):
         lis[index] = 0
         index -= 1
@@ -14,14 +14,18 @@ def count_slice(lis):
     lis[index] = (lis[index] + 1) % len(letters)
     return lis
 
-while True:
-    """
-    change the if statment to any valid thing you want
-    """
-    password = [letters[i] for i in len_pass]
-    print("".join(password))
+def Brute_Forec(letters,len_pass):
+    
+    while True:
+        """
+        change the if statment to any valid thing you want
+        """
+        password = [letters[i] for i in len_pass]
+        print("".join(password))
 
-    if "".join(password) == (letters[-1]*len(len_pass)):
-        break
+        if "".join(password) == (letters[-1]*len(len_pass)):
+            break
 
-    len_pass = count_slice(len_pass)
+        len_pass = count_slice(len_pass)
+
+Brute_Forec(letters,len_pass)
